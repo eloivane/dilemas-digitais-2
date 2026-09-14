95372c3346d277d8fe21799434ccc70731480ad4
eloivane <eloivane.ferreira@escola.pr.gov.br>
2026-09-14 08:07:17 -0300

    html

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 function corrigirQuiz(){
 
-
 let pontos = 0;
 
 
-let respostas = [
+let respostas=[
 
 document.querySelector('input[name="q1"]:checked'),
 
-document.querySelector('input[name="q2"]:checked'),
-
-document.querySelector('input[name="q3"]:checked')
+document.querySelector('input[name="q2"]:checked')
 
 ];
 
 
-
 respostas.forEach(function(resposta){
 
-if(resposta && resposta.value === "1"){
+if(resposta && resposta.value=="1"){
 
 pontos++;
 
@@ -27,35 +23,8 @@ pontos++;
 });
 
 
-
-let mensagem;
-
-
-
-if(pontos === 3){
-
-mensagem="🏆 Parabéns! Você é um especialista em segurança digital!";
-
-}
-
-else if(pontos === 2){
-
-mensagem="👏 Muito bem! Você está aprendendo bastante.";
-
-}
-
-else{
-
-mensagem="📚 Continue aprendendo. A internet fica mais segura quando usamos com cuidado.";
-
-}
-
-
-
 document.getElementById("resultado").innerHTML=
 
-"Você acertou " + pontos + " de 3 perguntas!<br><br>" + mensagem;
-
-
+"🎉 Você acertou "+pontos+" de 2 perguntas!";
 
 }
